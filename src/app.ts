05c7ef3367ab0d9fb4a5e37c274d7618349dab27
Nirob Hasan <nirob427417@gmail.com>
2023-05-12 11:24:24 +0600

    interface and schema

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,93 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors'
+import { Schema } from 'mongoose';
 
 const app: Application = express();
 
 //using cors
 app.use(cors());
 
+//perse cors
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+
+    //creating an interface
+    interface IUser {
+        id: string;
+        role: "student";
+        password: string;
+        name: {
+            firstName: string;
+            middleName?: string;
+            lastName: string;
+        };
+        dateOfBirth?: string;
+        gender: "male" | "female";
+        email?: string;
+        contactNo: string;
+        emergencyContactNo: string;
+        presentAddress: string;
+        permanentAddress: string;
+    }
+    // Create a Schema corresponding to the document interface.
+    const userSchema = new Schema<IUser>({
+        id: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        role: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        name: {
+            firstName: {
+                type: String,
+                required: true,
+            },
+            middleName: {
+                type: String,
+            },
+            lastName: {
+                type: String,
+                required: true,
+            },
+        },
+        dateOfBirth: {
+            type: String,
+        },
+        gender: {
+            type: String,
+            enum: ["male", "female"],
+        },
+        email: {
+            type: String,
+        },
+        contactNo: {
+            type: String,
+            required: true,
+        },
+        emergencyContactNo: {
+            type: String,
+            required: true,
+        },
+        presentAddress: {
+            type: String,
+            required: true,
+        },
+        permanentAddress: {
+            type: String,
+            required: true,
+        },
+    });
 })
+
+
 
 export default app;
