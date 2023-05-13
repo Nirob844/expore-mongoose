fb68e08bc93ed7c1e1bb4634cdf63e487c572d60
Nirob Hasan <nirob427417@gmail.com>
2023-05-13 11:11:08 +0600

    Create and Test Post Route

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,22 +1,29 @@
+import { IUser } from "./user.interface";
 import User from "./user.model";
 
-export const createUserToDb = async () => {
-    const user = await new User({
-        id: '666',
-        role: "student",
-        password: 'nirob',
-        name: {
-            firstName: 'Nirob',
-            lastName: 'Hasan',
-        },
-        gender: "male",
-        email: 'nirob@ahn.com',
-        contactNo: '01785541',
-        emergencyContactNo: '01785541',
-        presentAddress: 'Dhaka',
-        permanentAddress: 'Dhaka',
-    });
+export const createUserToDb = async (payload: IUser): Promise<IUser> => {
+    const user = await new User(payload);
     await user.save();
     return user;
-
 };
+
+export const getUserToDb = async (): Promise<IUser[]> => {
+    const users = await User.find();
+    return users;
+}
+
+  // const user = await new User({
+    //     id: '666',
+    //     role: "student",
+    //     password: 'nirob',
+    //     name: {
+    //         firstName: 'Nirob',
+    //         lastName: 'Hasan',
+    //     },
+    //     gender: "male",
+    //     email: 'nirob@ahn.com',
+    //     contactNo: '01785541',
+    //     emergencyContactNo: '01785541',
+    //     presentAddress: 'Dhaka',
+    //     permanentAddress: 'Dhaka',
+    // });
